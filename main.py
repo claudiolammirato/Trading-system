c7bfe873f20c601d46fe63c0ffbcd14bc284361b
claudiolammirato <claudio.lammirato@gmail.com>
2021-10-12 12:37:31 +0200

    0.0

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import no_type_check
+import datetime
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import yfinance as yf
+
+#library settings
+matplotlib.use('TkAgg')
+pd.options.display.max_rows = 99999
+
+def load_data_daily():
+    data  = yf.download('AAPL', start='2019-01-01', end='2021-06-12', progress=False,)
+    data.columns = ["date", "open", "high", "low", "close", "volume"]
+    data["dayofweek"] = data.index.dayofweek
+    data["day"] = data.index.day
+    data["month"] = data.index.month
+    data["yesr"] = data.index.year
+    data["dayofyear"] = data.index.dayofyear
+    data["quarter"] = data.index.quarter
+    return data
+    
+
+
+def main():
+    data = load_data_daily()
+    print(data.tail(10))
+    
+
+if __name__ == "__main__":
+    main()
+
